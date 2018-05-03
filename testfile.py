95d96e98ebe213133705b63d9d24b52fd1982589
Rye Buckley <rbuckle1@swarthmore.edu>
2018-05-03 12:34:41 -0400

    figuring a few things out

--- testfile.py
+++ testfile.py
@@ -1,17 +1,44 @@
 from subprocess import *
+def main():
 
-# subprocess.run(["echo", "'bird'", "|", "apertium", "-d", "developed_resources/apertium-en-es/", "en-es"])
-# subprocess.run(["echo", "'bird' | apertium -d developed_resources/apertium-en-es/ en-es"])
-# subprocess.run(["ls", "-l"])
-p1 = Popen(["echo", "bird"], stdout=PIPE)
-p2 = Popen(["apertium", "-d", "developed_resources/apertium-en-es/", "en-es"], stdin=p1.stdout, stdout=PIPE)
-p1.stdout.close()
-output=p2.communicate()[0]
-print(output.decode())
+    p1 = Popen(["cat", "corpora/littleredridinghood.es"], stdout=PIPE)
+    p2 = Popen(["apertium", "-d", "developed_resources/apertium-en-es/", "es-en-postchunk"], stdin=p1.stdout, stdout=PIPE)
+    p1.stdout.close()
+    output=p2.communicate()[0]
+    output = output.decode()
+    output = output.split("^")
+    print(output[3])
 
+    p1 = Popen(["cat", "corpora/littleredridinghood.en"], stdout=PIPE)
+    p2 = Popen(["apertium", "-d", "developed_resources/apertium-eng/", "eng-tagger"], stdin=p1.stdout, stdout=PIPE)
+    p1.stdout.close()
+    output1=p2.communicate()[0]
+    output1 = output1.decode()
+    output1 = output1.split("^")
+    print(output1[3])
 
-p1 = Popen(["cat", "corpora/littleredridinghood.eng"], stdout=PIPE)
-p2 = Popen(["apertium", "-d", "developed_resources/apertium-en-es/", "en-es-tagger"], stdin=p1.stdout, stdout=PIPE)
-p1.stdout.close()
-output=p2.communicate()[0]
-print(output.decode())
+    output, output1 = organize(output, output1)
+    write_to_file(output, output1)
+
+def organize(t1, t2):
+    """
+        try and line words up correctly
+    """
+    
+    return t1, t2
+
+def write_to_file(t1, t2):
+    """
+        write two lists to a file matching items
+    """
+    f = open("test.txt", "w")
+    for i in range(20):
+        f.write(t1[i])
+        f.write("\n")
+        f.write(t2[i])
+        f.write("\n")
+        f.write("\n")
+
+    f.close()
+
+main()
