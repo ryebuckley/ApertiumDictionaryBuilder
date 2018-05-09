6746b426d620dcda09b35ac18596e459b6cb4bc7
Alexander Robey <arobey1@cs.swarthmore.edu>
2018-05-09 13:52:59 -0400

    added case for untranslated word

--- word.py
+++ word.py
@@ -4,8 +4,9 @@ class Word(object):
     def __init__(self, raw, position):
         self.raw = raw
         self.position = position
+        print self.raw
         self.__get_attrs()
-        print (self.word, self.analyses)
+        # print (self.word, self.analyses)
 
     def __str__(self):
         return self.word
@@ -19,27 +20,33 @@ class Word(object):
         returns: nothing
         """
 
-        # if line comes from tagger
-        if '/' in self.raw:
-            parsed = self.raw.split('/')
-            if len(parsed) > 1:
-                parsed = parsed[1:]
+        if self.raw[0] == '*':
+            self.word = self.raw
+            self.analyses = [[]]
 
-            start_idx = parsed[0].index('<')
-            self.word = parsed[0][:start_idx]
-
-        # if line comes from translator
         else:
-            start_idx = self.raw.index('<')
-            self.word = self.raw[:start_idx]
-            parsed = [self.raw[start_idx:]]
 
-        self.analyses = []
-        for i in range(len(parsed)):
-            start_idx = parsed[i].index('<')
-            analysis = parsed[i][start_idx+1:].replace('>', '')
-            analysis = analysis.replace('$', '')
-            analysis = analysis.replace(' ', '')
-            split_pos = analysis.split('<')
+            # if line comes from tagger
+            if '/' in self.raw:
+                parsed = self.raw.split('/')
+                if len(parsed) > 1:
+                    parsed = parsed[1:]
 
-            self.analyses.append(split_pos)
+                start_idx = parsed[0].index('<')
+                self.word = parsed[0][:start_idx]
+
+            # if line comes from translator
+            else:
+                start_idx = self.raw.index('<')
+                self.word = self.raw[:start_idx]
+                parsed = [self.raw[start_idx:]]
+
+            self.analyses = []
+            for i in range(len(parsed)):
+                start_idx = parsed[i].index('<')
+                analysis = parsed[i][start_idx+1:].replace('>', '')
+                analysis = analysis.replace('$', '')
+                analysis = analysis.replace(' ', '')
+                split_pos = analysis.split('<')
+
+                self.analyses.append(split_pos)
